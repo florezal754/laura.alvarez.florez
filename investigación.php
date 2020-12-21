74ac233445ea5b7a50cdd05293a3efe7f2b6bc34
Laura <florezal754@gmail.com>
2020-12-21 12:39:30 +0100

    Updated index and create php files
    updated index.html
    created 3 php files

---
+++ investigación.php
@@ -0,0 +1,24 @@
+echo" ";echo" 
+<html lang="es">
+<head>
+  <!-- -->
+  <title></title>
+  <meta charset="utf-8">
+  <meta name="description" content="Este es un documento HTML5">
+  <link rel="stylesheet" href="css/estilos.css" TYPE="text/css">
+  <style>
+  </style>
+</head>
+<body>
+ <header>
+  <h1>investigación</h1>
+ </header>
+ <nav>
+ </nav>
+ <main>
+ </main>
+ <footer>
+ </footer>
+</body>
+</html>
+";
---
+++ obras.php
@@ -0,0 +1,24 @@
+echo" 
+<html lang="es">
+<head>
+  <!-- -->
+  <title></title>
+  <meta charset="utf-8">
+  <meta name="description" content="Este es un documento HTML5">
+  <link rel="stylesheet" href="css/estilos.css" TYPE="text/css">
+  <style>
+  </style>
+</head>
+<body>
+ <header>
+  <h1>obras</h1>
+ </header>
+ <nav>
+ </nav>
+ <main>
+ </main>
+ <footer>
+ </footer>
+</body>
+</html>
+";
