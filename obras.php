531b09437d404670c0301bd26ee8ca7af5d095f4
Laura <florezal754@gmail.com>
2020-12-21 12:53:52 +0100

    Updated index y obras

--- obras.php
+++ obras.php
@@ -1,24 +1,20 @@
-<?php echo" 
-<html lang="es">
-<head>
-  <!-- -->
-  <title></title>
-  <meta charset="utf-8">
-  <meta name="description" content="Este es un documento HTML5">
-  <link rel="stylesheet" href="css/estilos.css" TYPE="text/css">
-  <style>
-  </style>
-</head>
-<body>
- <header>
-  <h1>obras</h1>
- </header>
- <nav>
- </nav>
- <main>
- </main>
- <footer>
- </footer>
-</body>
-</html>
-";?>
+        <?php
+
+echo "<h1> TÍTULO </h1><br/>"; 
+
+echo "<p> Texto dela página .... </p>";
+
+echo "<p> Texto dela página .... </p>";
+
+echo "<p> Texto dela página .... </p>";
+
+
+
+echo "<p> Texto dela página .... </p>
+         <h1> Holaaaaaa .... </h1>
+     <h2> Texto dela página .... </h2>
+     ";
+
+
+
+?>
